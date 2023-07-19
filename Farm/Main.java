34f1c39f265b8bca36fe7e2dcab1557d15304362
lee <leeparker@outlook.com>
2023-07-19 14:21:09 +0100

    wednesday morning work

---
+++ Farm/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+    Panda panda = new Panda(true, "bamboo",
+
+            )
+    }
+}
---
+++ Farm/Panda.java
@@ -0,0 +1,9 @@
+public class Panda extends Bear{
+
+ String whereItLives;
+
+    public Panda(boolean knows_kung_fu, String eats, String whereItLives) {
+        super(knows_kung_fu, eats);
+        this.whereItLives = whereItLives;
+    }
+};
